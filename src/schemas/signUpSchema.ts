6825eca99d7818cade1348e63f9e034beae5999f
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2025-01-15 01:48:01 +0500

    verifyschema added

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -1,7 +1,13 @@
 import { z } from "zod";
 
 export const usernameValidation = z
-.string()
-.min(5, "username must be atleast 5 characters")
-.max(20, "username must be no more than 20 characters")
-.regex(/^[a-zA-Z0-9_]+$/, "username must not contain special character")
+    .string()
+    .min(5, "username must be at least 5 characters")
+    .max(20, "username must be no more than 20 characters")
+    .regex(/^[a-zA-Z0-9_]+$/, "username must not contain special character")
+
+export const signUpSchema = z.object({
+    username: usernameValidation,
+    email: z.string().email({ message: "Invalid email address" }),
+    password: z.string().min(6, { message: "password must be at least 6 characters" })
+})
