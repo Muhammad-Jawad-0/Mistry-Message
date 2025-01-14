5536a165c21c970cd1dbf629cf27081b8ce884a1
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2025-01-15 01:11:22 +0500

    modify

--- src/model/User.ts
+++ src/model/User.ts
@@ -68,3 +68,7 @@ const UserSchema: Schema<User> = new Schema({
     messages: [MassageSchema]
 
 })
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema);
+
+export default UserModel;
