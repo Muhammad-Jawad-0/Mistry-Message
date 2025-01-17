df1e6e5deff55de766ef0766e84de20f0197a6b2
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2025-01-18 03:01:33 +0500

    code added

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -8,6 +8,24 @@ export const POST = async (resquest: Request) => {
     await dbConnect()
     try {
         const { username, email, password } = await resquest.json()
+
+        const existingUserVerifiedByUsername = await UserModel.findOne({
+            username,
+            isVerified: true
+        })
+
+        const existingUserByEmail = await UserModel.findOne({ email })
+
+        if(existingUserByEmail){
+            
+        }
+
+        if (existingUserVerifiedByUsername) {
+            return Response.json({
+                success: false,
+                message: "Username is already taken"
+            }, { status: 400 })
+        }
     } catch (error) {
         console.error("Error Registering User", error)
         return Response.json(
