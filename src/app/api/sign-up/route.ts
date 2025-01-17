c7dea92710a2c478ae32dd0e0ec291c68292c3ee
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2025-01-18 02:49:34 +0500

    POST route

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -1,2 +1,23 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
+import bcrypt from "bcryptjs";
+
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+
+export const POST = async (resquest: Request) => {
+    await dbConnect()
+    try {
+        const { username, email, password } = await resquest.json()
+    } catch (error) {
+        console.error("Error Registering User", error)
+        return Response.json(
+            {
+                success: false,
+                message: "Error Registering User"
+            },
+            {
+                status: 500
+            }
+        )
+    }
+}
