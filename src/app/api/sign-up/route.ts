d51fb906224a9bbd8ff54b29a20b167dc4cab793
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2025-01-19 02:05:15 +0500

    route signin code

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -13,19 +13,37 @@ export const POST = async (resquest: Request) => {
             username,
             isVerified: true
         })
-
-        const existingUserByEmail = await UserModel.findOne({ email })
-
-        if(existingUserByEmail){
-            
-        }
-
         if (existingUserVerifiedByUsername) {
             return Response.json({
                 success: false,
                 message: "Username is already taken"
             }, { status: 400 })
         }
+
+        const existingUserByEmail = await UserModel.findOne({ email })
+
+        const verifyCode = Math.floor(100000 + Math.random() * 900000).toString()
+
+        if (existingUserByEmail) {
+
+        } else {
+            const hasedPassword = await bcrypt.hash(password, 10)
+            const expiryDate = new Date();
+            expiryDate.setHours(expiryDate.getHours() + 1)
+
+            const newUser = new UserModel({
+                username,
+                email,
+                password: hasedPassword,
+                verifyCode,
+                verifyCodeExpiry: expiryDate,
+                isVerified: false,
+                isAcceptingMessage: true,
+                messages: []
+            })
+        }
+
+
     } catch (error) {
         console.error("Error Registering User", error)
         return Response.json(
